cdd0bf62a49075a4b9ab0ec1a4c91c2a02b76c3d
Nandini Patel <156888692+nandinipatel183@users.noreply.github.com>
2026-03-26 17:19:58 +0530

    Time: 0 ms (100%), Space: 48.5 MB (24.11%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int n=nums.length;
+        int low=0,high=n-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(target==nums[mid]){
+                return mid;
+            }
+            else if(target>nums[mid]){
+                low=mid+1;
+            }
+            else{
+               high=mid-1;
+            }
+        }
+        return -1;
+    }
+}
