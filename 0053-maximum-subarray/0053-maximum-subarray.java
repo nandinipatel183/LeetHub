211314cb691dba71b91b67de266de761ebadc521
Nandini Patel <156888692+nandinipatel183@users.noreply.github.com>
2026-08-15 17:46:21 +0530

    Time: 1 ms (99.95%), Space: 77 MB (93.11%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,21 +1,15 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-
-        int sum = 0;
-        int maxi = nums[0];
-
-        for(int i = 0; i < nums.length; i++){
-            sum += nums[i];
-
-            if(sum > maxi){
-                maxi = sum;
-            }
-
-            if(sum < 0){
-                sum = 0;
-            }
+      int n=nums.length;
+      int maxsum=Integer.MIN_VALUE;
+      int currsum=0;
+      for(int i=0;i<n;i++){
+           currsum+=nums[i];
+           maxsum= Math.max(maxsum,currsum);
+           if(currsum<0){
+            currsum=0;
+           }
         }
-
-        return maxi;
+      return maxsum;
     }
 }
