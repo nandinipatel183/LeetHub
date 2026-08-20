f9f69009bf51b9f2aa36b6d3beb02b776cdbe4d5
Nandini Patel <156888692+nandinipatel183@users.noreply.github.com>
2026-08-20 18:49:51 +0530

    Time: 0 ms (100%), Space: 46 MB (10.1%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -15,16 +15,21 @@
  */
 class Solution {
     public boolean isBalanced(TreeNode root) {
-        return balanced(root)!=-1;
+        if(root==null){
+            return true;
+        }
+        int left=height(root.left);
+        int right=height(root.right);
+        if (Math.abs(left-right) <= 1 && isBalanced(root.left) && isBalanced(root.right)) {
+            return true;
+        }
+        return false;
     }
-    public int balanced(TreeNode root){
-      if (root == null) return 0;
-      int left=balanced(root.left);
-      if(left==-1)return -1;
-      int right=balanced(root.right);
-      if(right==-1)return -1;
-      if(Math.abs(left-right)>1)return -1;
-      return Math.max(left, right) + 1;
+    public int height(TreeNode root){
+       if(root==null)return 0;
+       int left=height(root.left);
+       int right=height(root.right);
+       return Math.max(left,right)+1;
     }
         
     }
