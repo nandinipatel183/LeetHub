89343b5acf85cff12809b35921c927138a203cb4
Nandini Patel <156888692+nandinipatel183@users.noreply.github.com>
2026-09-01 17:00:17 +0530

    Time: 29 ms (13.33%), Space: 52.8 MB (15.18%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -1,27 +1,16 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-         return atMost(nums, goal) - atMost(nums, goal - 1);
-    }
-
-    private int atMost(int[] nums, int goal) {
-
+        HashMap<Integer,Integer> mapp=new HashMap<>();
         int count=0;
-        int n=nums.length;
-        int left=0;
         int sum=0;
-        if(goal<0){
-            return 0;
+        mapp.put(0,1);
+        for(int num:nums){
+         sum+=num;
+        if(mapp.containsKey(sum-goal)){
+            count+=mapp.get(sum-goal);
         }
-        for(int right=0;right<n;right++){
-            sum+=nums[right];
-            while(sum>goal){
-                sum-=nums[left];
-                left++;
-            }
-            count+=right-left+1;
-        
+        mapp.put(sum,mapp.getOrDefault(sum,0)+1);
         }
         return count;
-
     }
 }
